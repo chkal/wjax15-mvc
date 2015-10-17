b1b6816d3dc241597c07f2bbc92d2d67c6dafd83
Christian Kaltepoth <christian@kaltepoth.de>
2015-10-17 12:55:38 +0200

    Thymeleaf example...

--- mvc-demo/src/main/java/de/chkal/mvc/thymeleaf/ThymeleafController.java
+++ mvc-demo/src/main/java/de/chkal/mvc/thymeleaf/ThymeleafController.java
@@ -6,7 +6,6 @@ import javax.mvc.annotation.Controller;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import java.util.Arrays;
-import java.util.List;
 
 @Controller
 @Path("/thymeleaf")
@@ -18,13 +17,10 @@ public class ThymeleafController {
   @GET
   public String render() {
 
-    List<String> messages = Arrays.asList(
-      "Text #1",
-      "Text #2",
-      "Text #3"
-    );
+    models.put( "messages", Arrays.asList(
+      "Text #1", "Text #2", "Text #3"
+    ) );
 
-    models.put( "messages", messages );
     return "thymeleaf.html";
 
   }
